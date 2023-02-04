957d640a7ff5f22e6b8fd5e15700e262e21a1b1a
Brad Coleman <bradcoleman60@gmail.com>
2023-02-03 20:24:30 -0800

    adds database schema and seeds database

--- models/index.js
+++ models/index.js
@@ -4,13 +4,44 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+
+// Categories have many Products AND Products belongsTo Category
+
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'CASCADE'
+})
+
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+})
+
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag,{
+  through: 'ProductTag',
+  foreignKey: 'id'
+})
+
+// ProductTag.belongsTo(Product,{
+//   foreignKey: 'product_id'
+// })
+
+
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, { 
+  through: 'ProductTag',
+  foreignKey: 'prod_id'
+
+});
+
+// ProductTag.belongsTo(Tag, {
+//   foreignKey: 'id'
+// })
 
 module.exports = {
   Product,
